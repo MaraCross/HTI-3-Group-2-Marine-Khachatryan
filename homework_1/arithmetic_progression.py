82972cb0420ced69a282ab4e891089dfcb3eb149
Mar-X03 <mari.yan75@yahoo.com>
2021-01-10 22:57:13 +0400

    progression

---
+++ homework_1/arithmetic_progression.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+index = int(input("enter third number: "))
+d = num2 - num1
+result = num1 + (index - 1) * d
+print(result) 
